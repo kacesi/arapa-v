3146e0ba6aee643a59a93b43587b1beacaaf4f7c
kacesi <carrionnavarrojoaquin@gmail.com>
2026-02-12 18:17:37 -0500

    arapa.py

--- arapa.py
+++ arapa.py
@@ -10,9 +10,9 @@ col1, col2 = st.columns(2)
 
 with col1:
     if st.button("cabrazo") ("paletón") ("gordo onicham"):
-    st.success("AY QUE RICO UWUWUWUWWU WAAAAAAAEH OEEEH")
-    st.image("https://photos.app.goo.gl/66Lbif9iBWH1MuYR8")
+        st.success("AY QUE RICO UWUWUWUWWU WAAAAAAAEH OEEEH")
+        st.image("https://photos.app.goo.gl/66Lbif9iBWH1MuYR8")
 with col2:
     if st.button("Claro que no")("es 100 por ciento hetero"):
-    st.error("Di que si mrd waaehh")
-    st.image("https://photos.app.goo.gl/zoaD5jPbXySCnPSP9", ":v")
+        st.error("Di que si mrd waaehh")
+        st.image("https://photos.app.goo.gl/zoaD5jPbXySCnPSP9", ":v")
