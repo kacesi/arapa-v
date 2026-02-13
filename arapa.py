041bef1c64dee009a046897ba0a2bc0cb72b7252
kacesi <carrionnavarrojoaquin@gmail.com>
2026-02-12 19:10:10 -0500

    arapa.py

--- arapa.py
+++ arapa.py
@@ -2,17 +2,14 @@ import streamlit as st
 st.set_page_config(page_title="Privado beibi",page_icon="😎")
 st.title("Acceso restringido")
 clave_usuario = st.text_input("Introduce la clave jeje:", type ="password")
-
 if clave_usuario == "arapacabro":
     st.success("waaaeh")
 # Configuración para que el link se vea oficial en Instagram
     st.set_page_config(page_title="arapa eres el de la foto?", page_icon="😔")
-
     st.title("Arapa es cabro paletón?")
     st.write("presiona beibe")
 #Creamos dos columnas para que los botontes estén uno al lado del otro
     col1, col2 = st.columns(2)
-
     with col1:
     if st.button("cabrazo","paletón","gordo onicham"):
         st.success("AY QUE RICO UWUWUWUWWU WAAAAAAAEH OEEEH")
@@ -25,6 +22,7 @@ if clave_usuario =="":
     st.info("Introduce la clave jeje:")
 else:
     st.error("Esa no es la contraseña papu")
+
 
 
 
